f50c46157e7936e966908a74d513bc29134696ff
khairahscorner <airahyusuff@gmail.com>
2023-03-10 16:48:10 +0000

    wk5 - exercise 5

--- week5/script.js
+++ week5/script.js
@@ -1,12 +1,16 @@
 function loadFunc() {
-  const el = document.getElementById("circle");
+  let el = document.getElementById("circle");
   el.addEventListener("click", showAlert);
   el.style.backgroundColor = "blue";
-  el.style.opacity = 0.5;
+  el.style.opacity = 1.0;
 }
 
 function showAlert() {
-  alert("The button was pressed");
+  // alert("The button was pressed");
+  let el = document.getElementById("circle");
+  var circleOpacity = parseFloat(el.style.opacity);
+
+  el.style.opacity = circleOpacity - 0.2;
 }
 
 document.addEventListener("DOMContentLoaded", loadFunc);
