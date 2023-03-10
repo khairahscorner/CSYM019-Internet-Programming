d9a23570884a3e786c6d9e4332ee2481b96b62dc
khairahscorner <airahyusuff@gmail.com>
2023-03-10 17:11:25 +0000

    wk5 - ex. 6 use interval func to decrease opacity

--- week5/script.js
+++ week5/script.js
@@ -1,16 +1,23 @@
 function loadFunc() {
   let el = document.getElementById("circle");
-  el.addEventListener("click", showAlert);
+  el.addEventListener("click", intervalFunc);
   el.style.backgroundColor = "blue";
   el.style.opacity = 1.0;
 }
 
 function showAlert() {
-  // alert("The button was pressed");
+  alert("The button was pressed");
+}
+
+function decreaseOpacity() {
   let el = document.getElementById("circle");
   var circleOpacity = parseFloat(el.style.opacity);
 
   el.style.opacity = circleOpacity - 0.2;
+}
+
+function intervalFunc() {
+  setInterval(decreaseOpacity, 1000);
 }
 
 document.addEventListener("DOMContentLoaded", loadFunc);
