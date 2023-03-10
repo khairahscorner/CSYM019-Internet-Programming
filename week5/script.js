6da1d940fbc1c38874d5a84bee3f970771abc166
khairahscorner <airahyusuff@gmail.com>
2023-03-10 16:37:29 +0000

    wk5 - exercise 2 + 3

--- week5/script.js
+++ week5/script.js
@@ -1,6 +1,8 @@
 function loadFunc() {
   const el = document.getElementById("circle");
   el.addEventListener("click", showAlert);
+  el.style.backgroundColor = "blue";
+  el.style.opacity = 0.5;
 }
 
 function showAlert() {
